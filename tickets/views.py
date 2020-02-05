dcb7f4d91cf792d68c426d13aef80753bbcb5066
dselivanovskaya <selivanovskaya.dasha@ru>
2020-02-05 19:55:34 +0300

    Now we can send tickets as pdf-files back

--- tickets/views.py
+++ tickets/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse, FileResponse, Http404
 from .models import Ticket
 
+import os
+
 # Create your views here.
 
 def show_tickets(request):
@@ -12,6 +14,7 @@ def show_tickets(request):
 def show_ticket_pdf(request, uid):
     ticket = Ticket.objects.get(id=uid)
     try:
-        return FileResponse(open(ticket.path, 'rb'), content_type='application/pdf')
+        return FileResponse(open(os.path.abspath(os.path.join('tickets',
+        ticket.path)), 'rb'), content_type='application/pdf')
     except FileNotFoundError:
         raise Http404
