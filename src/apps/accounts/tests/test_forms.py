4189a8f205ee107a1b3812bb77114a51a03e2860
Artyom Danilov <defytheflow@gmail.com>
2020-05-02 21:04:46 +0300

    Refactor accounts templates and improve tests
    Type: refactor, test

--- src/apps/accounts/tests/test_forms.py
+++ src/apps/accounts/tests/test_forms.py
@@ -2,9 +2,9 @@ from django.conf import settings
 from django.shortcuts import reverse
 from django.test import TestCase
 
-from tests.data import USER1
+from tests.data import USER_MALE
 
-from accounts.apps import AccountsConfig as app_conf
+from accounts.apps import AccountsConfig
 from accounts.forms import AccountLoginForm, AccountCreateForm
 from accounts.validators import CustomPasswordValidator
 
@@ -13,13 +13,13 @@ class AccountLoginFormTest(TestCase):
 
     @classmethod
     def setUpTestData(cls):
-        cls.view_url = reverse(app_conf.ACCOUNT_LOGIN_URL)
+        cls.view_url = reverse(AccountsConfig.ACCOUNT_LOGIN_URL)
         cls.form_class = AccountLoginForm
 
     def test_form_displays_correct_error_message_if_user_doesnt_exist(self):
         response = self.client.post(self.view_url, {
-            'username': USER1.username,
-            'password': USER1.password,
+            'username': USER_MALE.username,
+            'password': USER_MALE.password,
         })
         self.assertFormError(
             response, 'form', None,
@@ -31,16 +31,16 @@ class AccountCreateFormTest(TestCase):
 
     @classmethod
     def setUpTestData(cls):
-        cls.view_url = reverse(app_conf.ACCOUNT_CREATE_URL)
+        cls.view_url = reverse(AccountsConfig.ACCOUNT_CREATE_URL)
         cls.form_class = AccountCreateForm
         cls.validator_class = CustomPasswordValidator
 
     def test_form_is_invalid_if_password_doesnt_have_one_digit(self):
         form = self.form_class({
-            'first_name': USER1.first_name,
-            'last_name':  USER1.last_name,
-            'email':      USER1.email,
-            'username':   USER1.username,
+            'first_name': USER_MALE.first_name,
+            'last_name':  USER_MALE.last_name,
+            'email':      USER_MALE.email,
+            'username':   USER_MALE.username,
             'password1':  'nodigitshere',
             'password2':  'nodigitshere',
         })
@@ -48,10 +48,10 @@ class AccountCreateFormTest(TestCase):
 
     def test_form_is_invalid_if_password_doesnt_have_special_character(self):
         form = self.form_class({
-            'first_name': USER1.first_name,
-            'last_name':  USER1.last_name,
-            'email':      USER1.email,
-            'username':   USER1.username,
+            'first_name': USER_MALE.first_name,
+            'last_name':  USER_MALE.last_name,
+            'email':      USER_MALE.email,
+            'username':   USER_MALE.username,
             'password1': 'nospecialcharactershere123',
             'password2': 'nospecialcharactershere123',
         })
@@ -59,10 +59,10 @@ class AccountCreateFormTest(TestCase):
 
     def test_form_displays_correct_error_message_if_password_doesnt_have_one_digit(self):
         response = self.client.post(self.view_url, {
-            'first_name': USER1.first_name,
-            'last_name':  USER1.last_name,
-            'email':      USER1.email,
-            'username':   USER1.username,
+            'first_name': USER_MALE.first_name,
+            'last_name':  USER_MALE.last_name,
+            'email':      USER_MALE.email,
+            'username':   USER_MALE.username,
             'password1':  'nodigitshere',
             'password2':  'nodigitshere',
         })
@@ -73,10 +73,10 @@ class AccountCreateFormTest(TestCase):
 
     def test_form_displays_correct_error_message_if_password_doesnt_have_one_special_character(self):
         response = self.client.post(self.view_url, {
-            'first_name': USER1.first_name,
-            'last_name':  USER1.last_name,
-            'email':      USER1.email,
-            'username':   USER1.username,
+            'first_name': USER_MALE.first_name,
+            'last_name':  USER_MALE.last_name,
+            'email':      USER_MALE.email,
+            'username':   USER_MALE.username,
             'password1': 'nospecialcharactershere123',
             'password2': 'nospecialcharactershere123',
         })
--- src/apps/accounts/tests/test_signals.py
+++ src/apps/accounts/tests/test_signals.py
@@ -1,21 +1,21 @@
-from django.contrib.auth.models import User
 from django.test import TestCase
 
-from tests.data import USER1
+from tests.data import USER_MALE
 
 
 class AccountsSignalsTest(TestCase):
 
     @classmethod
     def setUpTestData(cls):
-        cls.user = USER1.create_in_db()
+        cls.user = USER_MALE.create_in_db()
 
     def test_on_successful_login_increment_login_count_is_called(self):
         before_login_count = self.user.profile.login_count
-        self.client.login(username=USER1.username, password=USER1.password)
+        self.client.login(username=USER_MALE.username, password=USER_MALE.password)
         self.user.refresh_from_db()
         self.assertEquals(before_login_count + 1, self.user.profile.login_count)
 
     def test_on_successful_login_initialize_session_entries_is_called(self):
-        self.client.login(username=USER1.username, password=USER1.password)
-        self.assertEquals(self.client.session['watched_tickets'], [])
+        self.client.login(username=USER_MALE.username, password=USER_MALE.password)
+        self.assertEquals(self.client.session['read_tickets'], [])
+        self.assertEquals(self.client.session['taken_quizzes'], {})
